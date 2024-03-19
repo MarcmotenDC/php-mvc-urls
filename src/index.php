9b85f22be59ad33b71e5f4cc5d7c1fd430c50526
MarcmotenDC <superman868789@gmail.com>
2024-03-19 12:29:33 -0500

    Fix ssh issues

--- src/index.php
+++ src/index.php
@@ -1,7 +1,14 @@
 <?php
 
-require "src/controllers/products.php";
+// get the controller and action from the query string
+$controller = $_GET["controller"];
+$action = $_GET["action"];
 
-$controller = new Products;
+// require the necessary controller using the variable value
+require "src/controllers/$controller.php";
 
-$controller->index();
+// assign the name of the desired controller to a $controller_object variable
+$controller_object = new $controller;
+
+// call the method from the controller using the $action value
+$controller_object->$action();
